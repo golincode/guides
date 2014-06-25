391af91fe0b02e4b621deb97ff079ef4a58b11c8
Gareth Bishop <gbishop@wearearchitect.com>
2014-06-25 15:13:37 +0100

    removed /Users/gareth.bishop nonsense

--- homestead_ruby.sh
+++ homestead_ruby.sh
@@ -1,13 +1,13 @@
 su - vagrant
 
-git clone https://github.com/sstephenson/rbenv.git {$HOME}/.rbenv
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> {$HOME}/.bashrc
-echo 'eval "$(rbenv init -)"' >> {$HOME}/.bashrc
-git clone https://github.com/sstephenson/ruby-build.git {$HOME}/.rbenv/plugins/ruby-build
-git clone https://github.com/sstephenson/rbenv-gem-rehash.git {$HOME}/.rbenv/plugins/rbenv-gem-rehash
-git clone https://github.com/maljub01/rbenv-bundle-exec.git {$HOME}/.rbenv/plugins/rbenv-bundle-exec
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+git clone https://github.com/sstephenson/rbenv-gem-rehash.git ~/.rbenv/plugins/rbenv-gem-rehash
+git clone https://github.com/maljub01/rbenv-bundle-exec.git ~/.rbenv/plugins/rbenv-bundle-exec
 
-source {$HOME}/.rbenv
+source ~/.rbenv
 
 rbenv install 2.1.1
 rbenv global 2.1.1
