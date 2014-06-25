f6d7bb4badee6b88b14dcdf404ef2ad09b2aadb1
Gareth Bishop <gbishop@wearearchitect.com>
2014-06-25 13:31:42 +0100

    Ruby install script for Homestead Vagrant box

---
+++ homestead_ruby.sh
@@ -0,0 +1,13 @@
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+git clone https://github.com/sstephenson/rbenv-gem-rehash.git ~/.rbenv/plugins/rbenv-gem-rehash
+git clone https://github.com/maljub01/rbenv-bundle-exec.git ~/.rbenv/plugins/rbenv-bundle-exec
+
+source ~/.bashrc
+
+rbenv install 2.1.1
+rbenv global 2.1.1
+
+gem install bundler
