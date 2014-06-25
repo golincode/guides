3834f6fffc191291f9453521ee50a197b2eaa545
Gareth Bishop <gbishop@wearearchitect.com>
2014-06-25 13:37:34 +0100

    another hopeful fix for rbenv

--- homestead_ruby.sh
+++ homestead_ruby.sh
@@ -5,7 +5,7 @@ echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 git clone https://github.com/sstephenson/rbenv-gem-rehash.git ~/.rbenv/plugins/rbenv-gem-rehash
 git clone https://github.com/maljub01/rbenv-bundle-exec.git ~/.rbenv/plugins/rbenv-bundle-exec
 
-exec $SHELL
+source ~/.rbenv
 
 rbenv install 2.1.1
 rbenv global 2.1.1
