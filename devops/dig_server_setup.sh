1d9307d769a38f0e937aa6ec6225a91ccdffac03
gbish <gbishop@wearearchitect.com>
2015-03-03 13:40:22 +0000

    copied ruby stuff from external file to this one

--- devops/dig_server_setup.sh
+++ devops/dig_server_setup.sh
@@ -9,19 +9,20 @@ yum -y update
 # install stuff we need, php/nginx/etc
 echo "Installing required packages..."
 yum install -y nginx php55 php55-cli php55-fpm php55-devel php55-common php55-mcrypt php55-mysqlnd php55-gd git
+                gcc openssl-devel libyaml-devel libffi-devel readline-devel zlib-devel gdbm-devel ncurses-devel
 
 if id -u "deploy" >/dev/null 2>&1; then
-	echo "User exists, skipping"
+  echo "User exists, skipping"
 else
-	echo "Creating user"
-	useradd -d /home/deploy -m deploy
-	mkdir /home/deploy/.ssh
-	touch /home/deploy/.ssh/authorized_keys
-	echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDQRmA5HU+HK0z63GAYHeJTWrFAvMeQRaJUS5kIrR+xzJfyz96zOTBEHRO0zH6OhYHTzQTdWapAdiYA4Ehf7lIXxRPvmNS2aYsRArjFa9Ct2TzeXxWZDlTgXuxS73Uj4aiiVRJCR0isB0PJcQbpUUbbcSHKeDJsL+qvZKrC+2CLHJ/5NLwvQ0ahYMkM5QY5mGrdEQWCGTd9aLoiCew9nftUDJcJsIOwCm5RY9Z+h2eA17tpuUtDYXmM6yiwnl6aSbGIKSqcMNWqLXWK6RThwBh+mnrL7Up9GIsOiocY8e3AGBuY4eRvCRrbGYchKdQx5rYF232z5zcivEdj+/DGkcrb gareth.bishop@LDNARC-NMX52VZ" >> /home/deploy/.ssh/authorized_keys
-	echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCsbGLLUaiMw2JM9f9Ox7vT73kKakpcAwbyoDrb3nb2/Jofna46wvfHWPROelCAS+USZkCRx26Jj9FTxAMHxB77sEh6jwxYUw0lFNdRI0uTrsigqODpnOMNZiW2okFA6WX6jfNqqpjJiL1Ew+DQk/qCsqb85IFPkn+LqWvkw2bzba1rEaKXumJNg+w63LaCX9CzgSCQR/eNrhp7gACxQNRfw68imVS3liiROC3QEv6CeRMFq07QGs39jBBUuXC44deFxo1Xi3UT0MjS6KfoIKxUd4scPoYPif3nsG4uBXalBAoBXJKVJffKF/SHnUoiZ7nwlBZXH4PaxXkxcmmaBfVZ edward.dakin@LDNARC-DMXP00B" >> /home/deploy/.ssh/authorized_keys
-	echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC/mdt1plQl6T41jVv8L/PL7ymEK3j5yLVVgQ76Lf6AV2DE1jsvD3fsNJ6dra7vGzxEd4ah3eNNm6x6YIDexPwkvOYTkkRevBAOWKWq3kVKpLCSS+zUceUv4xUl94nJAU+zhGV2PSPlyZO2SpcyntKIMLAoXxPwTZbvpq0sFEMD/dZZLkFKcWZI4mqe/jpDYL7ig3S8k3HVpbuMctBdTIHSDw1VJh5qTVheg6O7ODKnQzshPeEDp3NT7ZGubX6zZodsfXAk4uTBdOZJGW9URiJV5PpVFJMmPCf1BhyRx1B5/7DIMiiWq4o7Lx/iby7gL2Vh6Tlv8qoNn1lQ/ZRi299t adam.onishi@LDNARC-NMXN1VB" >> /home/deploy/.ssh/authorized_keys
+  echo "Creating user"
+  useradd -d /home/deploy -m deploy
+  mkdir /home/deploy/.ssh
+  touch /home/deploy/.ssh/authorized_keys
+  echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDQRmA5HU+HK0z63GAYHeJTWrFAvMeQRaJUS5kIrR+xzJfyz96zOTBEHRO0zH6OhYHTzQTdWapAdiYA4Ehf7lIXxRPvmNS2aYsRArjFa9Ct2TzeXxWZDlTgXuxS73Uj4aiiVRJCR0isB0PJcQbpUUbbcSHKeDJsL+qvZKrC+2CLHJ/5NLwvQ0ahYMkM5QY5mGrdEQWCGTd9aLoiCew9nftUDJcJsIOwCm5RY9Z+h2eA17tpuUtDYXmM6yiwnl6aSbGIKSqcMNWqLXWK6RThwBh+mnrL7Up9GIsOiocY8e3AGBuY4eRvCRrbGYchKdQx5rYF232z5zcivEdj+/DGkcrb gareth.bishop@LDNARC-NMX52VZ" >> /home/deploy/.ssh/authorized_keys
+  echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCsbGLLUaiMw2JM9f9Ox7vT73kKakpcAwbyoDrb3nb2/Jofna46wvfHWPROelCAS+USZkCRx26Jj9FTxAMHxB77sEh6jwxYUw0lFNdRI0uTrsigqODpnOMNZiW2okFA6WX6jfNqqpjJiL1Ew+DQk/qCsqb85IFPkn+LqWvkw2bzba1rEaKXumJNg+w63LaCX9CzgSCQR/eNrhp7gACxQNRfw68imVS3liiROC3QEv6CeRMFq07QGs39jBBUuXC44deFxo1Xi3UT0MjS6KfoIKxUd4scPoYPif3nsG4uBXalBAoBXJKVJffKF/SHnUoiZ7nwlBZXH4PaxXkxcmmaBfVZ edward.dakin@LDNARC-DMXP00B" >> /home/deploy/.ssh/authorized_keys
+  echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC/mdt1plQl6T41jVv8L/PL7ymEK3j5yLVVgQ76Lf6AV2DE1jsvD3fsNJ6dra7vGzxEd4ah3eNNm6x6YIDexPwkvOYTkkRevBAOWKWq3kVKpLCSS+zUceUv4xUl94nJAU+zhGV2PSPlyZO2SpcyntKIMLAoXxPwTZbvpq0sFEMD/dZZLkFKcWZI4mqe/jpDYL7ig3S8k3HVpbuMctBdTIHSDw1VJh5qTVheg6O7ODKnQzshPeEDp3NT7ZGubX6zZodsfXAk4uTBdOZJGW9URiJV5PpVFJMmPCf1BhyRx1B5/7DIMiiWq4o7Lx/iby7gL2Vh6Tlv8qoNn1lQ/ZRi299t adam.onishi@LDNARC-NMXN1VB" >> /home/deploy/.ssh/authorized_keys
   # refine this to what it needs
-	echo "deploy	ALL=(ALL:ALL) ALL" >> /etc/sudoers
+  echo "deploy  ALL=(ALL:ALL) ALL" >> /etc/sudoers
 fi
 
 # Add ll to profile
@@ -53,14 +54,20 @@ source ~/.bashrc
 nvm install 0.12.0
 nvm alias default 0.12.0
 
-# source the profile
-source ~/.bashrc
-
 # Install grunt
 npm install -g grunt-cli
 
 # Install Ruby 2.2.0 & Bundler
-curl https://raw.githubusercontent.com/wearearchitect/guides/master/ruby/rhel_ruby.sh | bash
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+
+git clone https://github.com/sstephenson/rbenv-gem-rehash.git ~/.rbenv/plugins/rbenv-gem-rehash
+git clone https://github.com/maljub01/rbenv-bundle-exec.git ~/.rbenv/plugins/rbenv-bundle-exec
+
+RUBY_CONFIGURE_OPTS=--enable-shared rbenv install 2.2.0
+rbenv global 2.2.0
 
 source ~/.bashrc
 
